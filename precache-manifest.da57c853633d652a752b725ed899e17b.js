20429324dfd11c9cce5e625dbeb968d88bc70fa7
Catalina Syddall <catalinasyddall@gmail.com>
2020-06-30 14:38:49 -0300

    Updates

---
+++ precache-manifest.da57c853633d652a752b725ed899e17b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad75bb3be86b504fb8cc7bd728ba96e1",
+    "url": "/shellow/index.html"
+  },
+  {
+    "revision": "851ed14b0bfd21e12f6b",
+    "url": "/shellow/static/css/main.84a1be68.chunk.css"
+  },
+  {
+    "revision": "43eea5aa26aecd1e8664",
+    "url": "/shellow/static/js/2.10d454ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shellow/static/js/2.10d454ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851ed14b0bfd21e12f6b",
+    "url": "/shellow/static/js/main.3c667a6c.chunk.js"
+  },
+  {
+    "revision": "5de87f01b9435d4bccc1",
+    "url": "/shellow/static/js/runtime-main.7c49fc97.js"
+  },
+  {
+    "revision": "bbbad9d84899d2497571164ffe58ca50",
+    "url": "/shellow/static/media/close.bbbad9d8.svg"
+  }
+]);
